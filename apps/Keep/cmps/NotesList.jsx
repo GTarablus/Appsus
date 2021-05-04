5b37969dcecdbb1f474484ac3c403b84dc0d8adb
RaphaelAssor <raphaelassor@rabcommerce.com>
2021-05-04 23:22:52 +0300

    first styling phase - notes layout

--- apps/Keep/cmps/NotesList.jsx
+++ apps/Keep/cmps/NotesList.jsx
@@ -1,7 +1,7 @@
 import {NotePreview} from '../cmps/NotePreview.jsx'
 export function NotesList({notes,onRemoveNote,onSaveNote}){
 
-    return <section>
+    return <section className="notes-container">
         {notes.map(note=><NotePreview note={note} key={note.id} onRemoveNote={onRemoveNote} onSaveNote={onSaveNote} />)}
     </section>
 }
